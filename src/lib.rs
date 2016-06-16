007960b29ce2a4a8bdfeaade629751f66fefa8aa
Ashley Williams <ashley666ashley@gmail.com>
2016-06-16 20:06:02 +0100

    implement approx equal, add one test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+#[macro_export]
+macro_rules! assert_approx_eq {
+    ($a:expr, $b:expr) => ({
+        let (a, b) = (&$a, &$b);
+        assert!((*a - *b).abs() < 1.0e-6,
+                "assertion failed: `(left !== right)` \
+                           (left: `{:?}`, right: `{:?}`)",
+                 *a, *b);
+    })
+}
+
+#[test]
+fn it_should_not_panic_if_values_are_approx_equal() {
+  assert_approx_eq!(64f32.sqrt(), 8f32);
 }
