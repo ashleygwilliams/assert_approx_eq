7bb74278b9d2945e2204644d45cf90d68c967aeb
Ashley Williams <ashley666ashley@gmail.com>
2017-03-27 11:59:36 -0400

    feat(log): show diff values in log message

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,12 @@
 #[macro_export]
 macro_rules! assert_approx_eq {
     ($a:expr, $b:expr) => ({
+        let eps = 1.0e-6;
         let (a, b) = (&$a, &$b);
-        assert!((*a - *b).abs() < 1.0e-6,
+        assert!((*a - *b).abs() < eps,
                 "assertion failed: `(left !== right)` \
-                           (left: `{:?}`, right: `{:?}`)",
-                 *a, *b);
+                           (left: `{:?}`, right: `{:?}`, expect diff: `{:?}`, real diff: `{:?}`)",
+                 *a, *b, eps, (*a - *b).abs());
     });
     ($a:expr, $b:expr, $eps:expr) => ({
         let (a, b) = (&$a, &$b);
