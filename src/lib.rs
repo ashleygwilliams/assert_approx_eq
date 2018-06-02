306dc5f61c7c59dbc243b836d6152d53cf7fb0a4
axesilo <d.matthew.carroll@gmail.com>
2018-06-02 06:43:12 -0400

    Fixed eps to only be evaluated once

--- src/lib.rs
+++ src/lib.rs
@@ -28,10 +28,11 @@ macro_rules! assert_approx_eq {
     });
     ($a:expr, $b:expr, $eps:expr) => ({
         let (a, b) = (&$a, &$b);
-        assert!((*a - *b).abs() < $eps,
+        let eps = $eps;
+        assert!((*a - *b).abs() < eps,
                 "assertion failed: `(left !== right)` \
                            (left: `{:?}`, right: `{:?}`, expect diff: `{:?}`, real diff: `{:?}`)",
-                 *a, *b, $eps, (*a - *b).abs());
+                 *a, *b, eps, (*a - *b).abs());
     })  
 }
 
